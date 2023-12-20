1be3a8862b3f858ee8b67c59ca7862ae0311f07a
Staniulionis <tautvydas.staniulionis@gmail.com>
2023-12-20 10:52:16 +0200

    mano

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
+
+
 public class LoginTest {
     WebDriver driver;
 
